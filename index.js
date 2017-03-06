e6dc9148985537109e65d58202bf2ad8b091eb9e
Arlin <arlin.grijalba@gmail.com>
2017-03-06 11:50:31 -0800

    First commit with html, js and Jquery already working

---
+++ index.js
@@ -0,0 +1,31 @@
+// Create a link to the location using the longitude and latitude provided.
+// (Example https://www.google.com/maps?q=33.09745,-116.99572)
+
+$(document).ready(function(){
+  var topSpots = $.getJSON("topspots.json", function(data){
+    for (var i=0; i<data.length; i++){
+      var name = data[i].name;
+      var description = data[i].description;
+      var location = createMapsLink(data[i].location);
+
+      var anchor = createAnchor(location);
+
+      var newTrow = "<tr>" +
+                      "<td>" + name + "</td>" +
+                      "<td>" + description + "</td>" +
+                      "<td>" + anchor + "</td>" +
+                    "</tr>";
+      $('#placesTable').append(newTrow);
+    }
+  });
+
+  function createMapsLink(location){
+    var latitude = location[0];
+    var longitude = location[1];
+    return ("https://www.google.com/maps?q=" + latitude + "," + longitude);
+  }
+
+  function createAnchor(location){
+    return "<a href='" + location + "'>Open in Google Maps!</a>";
+  }
+});
